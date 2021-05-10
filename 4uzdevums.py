b68ffc21e4d4689dfa597ef75b293ad968bf93ae
JanisARubezis <73471045+JanisARubezis@users.noreply.github.com>
2021-05-10 11:54:34 +0300

    iesniedzu

--- 4uzdevums.py
+++ 4uzdevums.py
@@ -2,3 +2,14 @@
 Uzrakstiet programmu Python, lai dotajā sarakstā saskaitītu 
 cik skaitļi 4 ir  dotajā sarakstā.
   """
+skaitļi = ["4", "5", "6", "4", "7", "8", "4"]
+i=0
+x=len(skaitļi)-1
+sk=0
+while i<=x:
+  if skaitļi[i]=="4":
+    sk= sk + 1
+    i= i+1
+  else:
+    i= i+1
+print(sk)
--- 6uzdevums.py
+++ 6uzdevums.py
@@ -1,3 +1,6 @@
 """
 Uzrakstiet programmu Python, lai parādītu pašreizējo datumu un laiku.
 """
+from datetime import date
+x=date.today()
+print(x)
