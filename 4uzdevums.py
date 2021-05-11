9df7a33707d7493da84316e731394fb758b38828
ojars856 <83339243+ojars856@users.noreply.github.com>
2021-05-11 08:52:41 +0300

    Update 4uzdevums.py

--- 4uzdevums.py
+++ 4uzdevums.py
@@ -1,4 +1,8 @@
 """
 Uzrakstiet programmu Python, lai dotajā sarakstā saskaitītu 
 cik skaitļi 4 ir  dotajā sarakstā.
-  """
+  
+ """
+skaitļi = [2,3,4,5,4,4,3,9,21,4,4,5,4]
+elm_count = skaitļi.count(4)
+print('Sarakstā skaitlis 4 atkārtojas', elm_count,'reizes')
