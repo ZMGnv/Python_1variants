30e7da953e7ccd1353c6da71bb92710d99079c5c
anceluka <73470979+anceluka@users.noreply.github.com>
2021-05-19 14:21:56 +0300

    kļūdu labojums
    iesniedzu uzdevumus, kuros bija kļūdas

--- 3uzdevums.py
+++ 3uzdevums.py
@@ -5,5 +5,12 @@ pirmās un pēdējās krāsas no šī saraksta.
 krasa_saraksts = ["sarkans", "zaļš", "balts", "melns"]
 """
 
-krasa_saraksts = ["sarkans", "zaļš", "balts", "melns"]
-print(krasa_saraksts [0], krasa_saraksts [3])
+krasa_saraksts=["sarkans", "zaļš", "balts", "melns"]
+
+viens=krasa_saraksts[0]
+pedeja=krasa_saraksts[3]
+
+print(viens,pedeja)
+
+skaits=len(krasa_saraksts)
+print(skaits)
